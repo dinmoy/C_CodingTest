dff18098c71e643c0c71589fb34d52ec359db59a
Hyojin Lim <111869216+dinmoy@users.noreply.github.com>
2023-03-29 10:16:52 +0900

    코드업 C언어 문제풀이

---
+++ Test0329_06.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int num;
+	scanf("%d",&num);
+	printf("%b",num); //c언어는 2진법 전환 못함 
+	printf("%o",num);
+	printf("%x",num);
+}
---
+++ Tset0329_07.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+	char c;
+	int b;
+	scanf("%s",&c); //1036¹ø 
+	printf("%d",c);
+	scanf("%d",&b); //1037¹ø 
+	printf("%c",b);
+} 
